03a465ccebe04415892a141fb083cb1f373e3b96
icicle <rajendra.chorsia@icicletech.com>
2012-11-24 17:18:12 +0530

    commited all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '3fbfcabab8e03704d276fd89044fb96fb0a7e7e638473014060b9a34f09cac66213612548fc57468d2f1c03a93e0bce7dd71390912112df07128c8ea6229ab65'
